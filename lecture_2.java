91600fc9d75634063a0119092f7d44b0d03fa669
surikunal <suri.kunal.222@gmail.com>
2022-03-21 14:35:19 +0530

    update

--- lecture_2.java
+++ lecture_2.java
@@ -137,22 +137,109 @@ public class lecture_2 {
 
         // ============================= cross pattern
 
-        int n = 9;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if ( (i == j) || (i + j == n - 1) )
-                    System.out.print("*\t");
-                else
-                    System.out.print("\t");
-            }
-            System.out.println();
+        // int n = 9;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if ( (i == j) || (i + j == n - 1) )
+        //             System.out.print("*\t");
+        //         else
+        //             System.out.print("\t");
+        //     }
+        //     System.out.println();
+        // }
+
+        // ===============================
+
+        // int n = 7;
+        // int count = 0;  // count the number of divisibles
+        // for (int i = 1; i <= n; i++) {
+        //     if (n % i == 0) {
+        //         count++;
+        //     }
+        // }
+        // if (count == 2) {
+        //     System.out.println(true);
+        // } else {
+        //     System.out.println(false);
+        // }
+
+        // ======================================
+        // fibonacci series
+        // int n = 100;
+        // int x = 0;
+        // int y = 1;
+        // for (int i = 0; i < n; i++) {
+        //     System.out.print(x + " ");
+        //     int z = x + y;
+        //     x = y;
+        //     y = z;
+        // }
+
+        // ===========================================================
+        // int n = 21430;
+        // // inverse of a number
+        // int inv = 0;    // answer
+        // int oldPower = 0;
+        // while ( n != 0 ) {
+            //     int oldDigit = n % 10;
+            //     int newDigit = oldPower;
+            //     int newPower = oldDigit;
+            
+            //     inv = inv + (int)Math.pow(10, newPower - 1);
+            
+            //     n = n / 10;
+            //     oldPower++;
+            // }
+            // System.out.println(inv);
+        // }
+        // ===========================================================
+            // Pythagoraus Triplet
+        // int a = 4, b = 3, c = 5;
+        // int max = 0;
+        // if (a >= max) {
+        //     max = a;
+        // }
+        // if (b >= max) {
+        //     max = b;
+        // }
+        // if (c >= max) {
+        //     max = c;
+        // }
+
+        // // max == 5
+        // if ( max == a ) {
+        //     if ( b * b + c * c == a * a ) 
+        //         System.out.print(true);
+        //     else 
+        //         System.out.print(false);
+        // } else if ( max == b ) {
+        //     if ( a * a + c * c == b * b ) 
+        //         System.out.print(true);
+        //     else 
+        //         System.out.print(false);
+        // } else if ( max == c ) {
+        //     if ( a * a + b * b == c * c ) 
+        //         System.out.print(true);
+        //     else 
+        //         System.out.print(false);
+        // }
+        // ========================================
+        // Prime Factorization
+        // int n = 1440;
+        // for (int div = 2; div < n; div++) {
+
+        //     while ( n % div == 0 ) {
+        //         n = n / div;
+        //         System.out.print(div + ", ");
+        //     }
+
+        // }
+
+        // =============================== bulbs
+        int n = 100;
+        for (int i = 1; i * i <= n ; i++) {
+            System.out.print(i * i);    // 1, 4, 9, 16, 25, 36, .........
         }
-
-
-
-
-
-
-
     }
 }
+    
