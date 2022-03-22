c95d004a111ed4aa00ba33d7225a62a7d11e65ba
surikunal <suri.kunal.222@gmail.com>
2022-03-22 13:28:10 +0530

    update

---
+++ lecture4/lecture.java
@@ -0,0 +1,59 @@
+import java.util.*;
+class lecture {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter size of array : ");
+        int n = scn.nextInt();  //input size of array
+        int[] arr = new int[n];
+        System.out.println("Enter values of array : ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        // =============================================
+        int[] ans = inverseOfArray(arr, n);   // calling a function
+        for (int i = 0; i < n; i++) {
+            System.out.println(ans[i]);
+        }
+    }
+    public static int[] inverseOfArray(int[] arr, int n) {
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            int val = arr[i];
+            answer[val] = i;
+        }
+        return answer;
+    }
+
+    // rotate an array by k  =============================================
+    public void swap(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+    
+    public void reverse(int[] arr, int si, int ei) {
+        while (si < ei) {
+            swap(arr, si, ei);
+            si++;
+            ei--;
+        }
+    }
+    
+    public void rotate(int[] arr, int k) {
+        // 3 steps
+        int n = arr.length;
+        // to handle negetive cases
+        if (k < 0)
+            k = k + n;
+        
+        // to handle long cases
+        k = k % n;
+        
+        // 1 -> reverse array by k size from last
+        reverse(arr, n - k, n - 1);  // method and functions
+        // 2 -> reverse the remaining array
+        reverse(arr, 0, n - k - 1);
+        // 3 -> reverse the whole array
+        reverse(arr, 0, n - 1);
+    }
+}
